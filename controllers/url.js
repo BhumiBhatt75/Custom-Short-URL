437c4185ffb8b309eb8a626c32a87e310648c58c
Bhumi Bhatt <shinedorrie.bookish55@gmail.com>
2026-02-10 14:56:38 +0530

    Update url.js

--- controllers/url.js
+++ controllers/url.js
@@ -2,7 +2,7 @@ const shortid = require("shortid");
 const URL = require('../models/url');
 
 async function handleGenerateNewShortURL(req, res) {
-  const { url } = req.body || {};   // ✅ SAFE LINE
+  const { url } = req.body || {}; 
 
   if (!url) {
     return res.status(400).json({ error: "url is required" });
